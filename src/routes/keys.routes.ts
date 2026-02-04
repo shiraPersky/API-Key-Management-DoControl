5c8b51e2e17026bc556a9c5fa71b1c7c8fd67dc4
Shira <shira.persky@gmail.com>
2026-02-04 20:57:10 +0200

    add route to revoke api key

--- src/routes/keys.routes.ts
+++ src/routes/keys.routes.ts
@@ -6,3 +6,4 @@ export const keysRouter = Router();
 //routes for api key per account
 keysRouter.post("/accounts/:accountId/keys", KeysController.create);//pass the request to the controller
 keysRouter.get("/accounts/:accountId/keys", KeysController.list);
+keysRouter.post("/accounts/:accountId/keys/:id/revoke",KeysController.revoke);
