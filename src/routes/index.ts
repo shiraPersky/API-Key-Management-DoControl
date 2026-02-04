a4a575a7a0034aedb276c2b1f5ae0972372dc98c
Shira <shira.persky@gmail.com>
2026-02-04 21:15:21 +0200

    docker compose

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,7 @@
 import { Router } from "express";
-import { pool } from "../database/pool";
 import { keysRouter } from "./keys.routes";
 
 export const routes = Router();
-
-routes.get("/try", (_req, res) => {
-  res.json({ status: "ok" });
-});
-
-routes.get("/debug/db-ping", async (_req, res) => {
-  const result = await pool.query("SELECT 1 as ok");
-  res.json({ db: "ok", result: result.rows[0] });
-});
 
 
 routes.use(keysRouter);
